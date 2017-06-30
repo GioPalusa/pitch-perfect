467d08abc4832bde40f3e9ed6a611cb65580170f
Giovanni Palusa <giovanni@palusa.se>
2017-06-30 22:31:04 +0200

    Fixed layout issues on iphone5/SE in landscape mode

--- pitch perfect/PlaySoundsViewController.swift
+++ pitch perfect/PlaySoundsViewController.swift
@@ -19,6 +19,7 @@ class PlaySoundsViewController: UIViewController {
     @IBOutlet weak var reverbButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
     
+    
     var recordedAudioURL:URL!
     var audioFile:AVAudioFile!
     var audioEngine:AVAudioEngine!
@@ -55,6 +56,13 @@ class PlaySoundsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupAudio()
+        snailButton.imageView?.contentMode = .scaleAspectFit
+        chipmunkButton.imageView?.contentMode = .scaleAspectFit
+        rabbitButton.imageView?.contentMode = .scaleAspectFit
+        vaderButton.imageView?.contentMode = .scaleAspectFit
+        echoButton.imageView?.contentMode = .scaleAspectFit
+        reverbButton.imageView?.contentMode = .scaleAspectFit
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
